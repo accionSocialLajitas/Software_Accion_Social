309c03f5073f5f3257a8e223e04783cf571c51d2
amadeo <amadeoescudero24@gmail.com>
2025-10-23 00:13:23 -0300

    commit inicial

---
+++ Software_Accion_Social/src/main/java/com/Software_Accio_Social/Software_Accion_Social/SoftwareAccionSocialApplication.java
@@ -0,0 +1,13 @@
+package com.Software_Accio_Social.Software_Accion_Social;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoftwareAccionSocialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoftwareAccionSocialApplication.class, args);
+	}
+
+}
---
+++ Software_Accion_Social/src/test/java/com/Software_Accio_Social/Software_Accion_Social/SoftwareAccionSocialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Software_Accio_Social.Software_Accion_Social;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoftwareAccionSocialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
