de4d56d586e209a45c25af4e6aabbae81e69354d
Matias <matuteluna114@gmail.com>
2025-11-03 11:41:55 -0300

    SGAS-14-Actualizacion de la entity Ubicacion

--- AccionSocial/src/main/java/Implement/UbicacionService.java
+++ AccionSocial/src/main/java/Implement/UbicacionService.java
@@ -7,7 +7,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import Entity.Ubicacion;
-import Entity.UsuarioSocial;
 import Repository.UbicacionRepository;
 import Service.InterUbicacion;
 
